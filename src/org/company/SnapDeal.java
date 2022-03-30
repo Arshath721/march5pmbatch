4a29ae07323758371d18e39ea0c6b9d8baa451e3
Sinkaravel097 <loophawk@gmail.com>
2022-03-30 18:04:22 +0530

    solved the conflict

--- src/org/company/SnapDeal.java
+++ src/org/company/SnapDeal.java
@@ -1,6 +1,12 @@
 package org.company;
 
 public class SnapDeal {
+	
+	private void emp1() {
+		System.out.println("emp done my program");
+
+	}
+	
 	private void emp2() {
 		System.out.println("emp 2 done my code");
 
